476ff9d4548bb03a5de8f0e00bc443c7885f7bd3
Mateus <Mateus@Mateus-PC>
2017-12-02 00:52:38 -0200

    New Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        <title></title>
+    </head>
+    <body>
+        <h1>Tasks</h1>
+        <a href="insert_task.php">Add Task</a>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
